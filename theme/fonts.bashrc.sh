fb411859cb06cf44ccbe8e2455fc2669aa58b157
Shane O'Connell <shane@oconnell.cc>
2014-05-05 14:00:45 -0400

    added shell function to enable/disable the powerline font usage in vimrc

---
+++ theme/fonts.bashrc.sh
@@ -0,0 +1,32 @@
+# vim: et ts=4 sw=4
+
+export POWERLINE_FONT=1
+
+function powerline_font {
+    if [ $# -ne 1 ]; then
+        read -r -p 'Do you have the powerline font installed? [Y/n] ' yn
+        if [ -z "$yn" ]; then
+            export POWERLINE_FONT=1
+            exit 0
+        fi
+    else
+        yn="$1"
+    fi
+
+    case "$yn" in
+        y)       export POWERLINE_FONT=1 ;;
+        Y)       export POWERLINE_FONT=1 ;;
+        yes)     export POWERLINE_FONT=1 ;;
+        1)       export POWERLINE_FONT=1 ;;
+        enable)  export POWERLINE_FONT=1 ;;
+
+        n)       export POWERLINE_FONT=0 ;;
+        N)       export POWERLINE_FONT=0 ;;
+        no)      export POWERLINE_FONT=0 ;;
+        0)       export POWERLINE_FONT=0 ;;
+        disable) export POWERLINE_FONT=0 ;;
+
+        *) echo "Error: unknown argument: $1"; exit 1 ;;
+    esac
+}
+export -f powerline_font
