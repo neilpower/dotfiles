bdee69a926f4aa5f5db32062a286ffb709d50558
Shane O'Connell <shane@oconnell.cc>
2014-07-04 12:03:44 -0400

    added ltr alias

--- bash/aliases.bashrc.sh
+++ bash/aliases.bashrc.sh
@@ -2,6 +2,8 @@ alias ls='ls -Fh --color=auto'
 alias la='ls -Fha --color=auto'
 alias ll='ls -Fhl --color=auto'
 alias lla='ls -Fhla --color=auto'
+alias ltr='ls -Fhltr --color=auto'
+
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
